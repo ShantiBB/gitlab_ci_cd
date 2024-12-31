abef89b423ab3f71b4a4de0fdaad2ee36af57c1c
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-12-31 14:41:43 +0300

    test changing

--- app/api/v1/routers/wallets.py
+++ app/api/v1/routers/wallets.py
@@ -27,8 +27,8 @@ async def create_wallet(
 @router.get("/")
 async def get_all_wallets(
     session: AsyncSessionDep,
-) -> list[WalletSchema]:
-    return await WalletDAO.get_all(session)
+) -> dict:
+    return {"status": "Ну получается запушил :)"}
 
 
 @router.get("/{wallet_id}/")
